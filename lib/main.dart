f1c1b82670a4bb0f07673827a27bd877df3afc62
ali12hhh-oss <ali12.hhh@gmail.com>
2026-07-13 20:26:48 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,24 @@
 import 'package:flutter/material.dart';
-import 'theme.dart';
 import 'screens/home_screen.dart';
+import 'theme.dart';
 
 void main() {
-  runApp(const DaleelZuwarApp());
+  runApp(const DaleelApp());
 }
 
-class DaleelZuwarApp extends StatefulWidget {
-  const DaleelZuwarApp({super.key});
+class DaleelApp extends StatefulWidget {
+  const DaleelApp({super.key});
 
   @override
-  State<DaleelZuwarApp> createState() => _DaleelZuwarAppState();
+  State<DaleelApp> createState() => _DaleelAppState();
 }
 
-class _DaleelZuwarAppState extends State<DaleelZuwarApp> {
-  // الوضع الافتراضي: يتبع إعدادات الجهاز (نهاري/ليلي)، ويمكن للمستخدم تبديله يدوياً
-  ThemeMode _themeMode = ThemeMode.system;
+class _DaleelAppState extends State<DaleelApp> {
+  ThemeMode _themeMode = ThemeMode.light;
 
   void _toggleTheme() {
     setState(() {
-      if (_themeMode == ThemeMode.light) {
-        _themeMode = ThemeMode.dark;
-      } else if (_themeMode == ThemeMode.dark) {
-        _themeMode = ThemeMode.light;
-      } else {
-        // إن كان يتبع النظام، اجعل أول ضغطة تنتقل إلى الوضع الليلي مباشرة
-        _themeMode = ThemeMode.dark;
-      }
+      _themeMode = _themeMode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
     });
   }
 
@@ -38,14 +30,6 @@ class _DaleelZuwarAppState extends State<DaleelZuwarApp> {
       theme: buildLightTheme(),
       darkTheme: buildDarkTheme(),
       themeMode: _themeMode,
-      locale: const Locale('ar', 'IQ'),
-      supportedLocales: const [Locale('ar', 'IQ')],
-      builder: (context, child) {
-        return Directionality(
-          textDirection: TextDirection.rtl,
-          child: child ?? const SizedBox.shrink(),
-        );
-      },
       home: HomeScreen(
         themeMode: _themeMode,
         onToggleTheme: _toggleTheme,
